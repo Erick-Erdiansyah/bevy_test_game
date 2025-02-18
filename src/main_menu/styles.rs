d32a19db7ef8072050633d8c7dd3bd0b32e46540
Erick-Erdiansyah <erdiansyah290@gmail.com>
2025-02-18 22:19:06 +0700

    chore:refactor style in main menu layout

--- src/main_menu/styles.rs
+++ src/main_menu/styles.rs
@@ -1,3 +1,67 @@
 use bevy::prelude::*;
 
-pub const NORMAL_BUTTON_COLOR:Color = Color::srgb(0.15, 0.15, 0.15);
+pub const NORMAL_BUTTON_COLOR: Color = Color::srgb(0.15, 0.15, 0.15);
+
+pub fn main_menu_style() -> Node {
+    Node {
+        flex_direction: FlexDirection::Column,
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        row_gap: Val::Px(8.0),
+        column_gap: Val::Px(8.0),
+        width: Val::Percent(100.0),
+        height: Val::Percent(100.0),
+        ..Node::DEFAULT
+    }
+}
+
+pub fn image_style() -> Node {
+    Node {
+        width: Val::Px(64.0),
+        height: Val::Px(64.0),
+        margin: UiRect {
+            left: Val::Px(8.0),
+            right: Val::Px(8.0),
+            top: Val::Px(8.0),
+            bottom: Val::Px(8.0),
+        },
+        ..Node::DEFAULT
+    }
+}
+
+pub fn button_style() -> Node {
+    Node {
+        align_items: AlignItems::Center,
+        justify_content: JustifyContent::Center,
+        width: Val::Px(200.0),
+        height: Val::Px(80.0),
+        ..Node::DEFAULT
+    }
+}
+
+pub fn title_style() -> Node {
+    Node {
+        flex_direction: FlexDirection::Row,
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        width: Val::Px(620.0),
+        height: Val::Px(120.0),
+        ..Node::DEFAULT
+    }
+}
+
+pub fn text_font_32(asset_server: &Res<AssetServer>) -> TextFont {
+    TextFont {
+        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+        font_size: 32.0,
+        ..Default::default()
+    }
+}
+
+pub fn text_font_64(asset_server: &Res<AssetServer>) -> TextFont {
+    TextFont {
+        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+        font_size: 32.0,
+        ..Default::default()
+    }
+}
--- src/main_menu/systems/layouts.rs
+++ src/main_menu/systems/layouts.rs
@@ -2,7 +2,7 @@ use bevy::{prelude::*, ui::widget::NodeImageMode};
 
 use crate::main_menu::{
     components::{MainMenu, PlayButton, QuitButton},
-    styles::NORMAL_BUTTON_COLOR,
+    styles::*,
 };
 
 pub fn spawn_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -15,97 +15,42 @@ pub fn despawn_main_menu(mut commands: Commands, main_menu_query: Query<Entity, 
 }
 pub fn build_main_menu(commands: &mut Commands, asset_server: &Res<AssetServer>) -> Entity {
     let main_menu_entity = commands
-        .spawn((
-            Node {
-                flex_direction: FlexDirection::Column,
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                row_gap: Val::Px(8.0),
-                column_gap: Val::Px(8.0),
-                width: Val::Percent(100.0),
-                height: Val::Percent(100.0),
-                ..Default::default()
-            },
-            MainMenu {},
-        ))
+        .spawn((main_menu_style(), MainMenu {}))
         .with_children(|parent| {
             // ======= ALSO NOT PLAY BUTTON =======
-            parent
-                .spawn(Node {
-                    flex_direction: FlexDirection::Row,
-                    justify_content: JustifyContent::Center,
-                    align_items: AlignItems::Center,
-                    width: Val::Px(620.0),
-                    height: Val::Px(120.0),
-                    ..Default::default()
-                })
-                .with_children(|parent| {
-                    // Image
-                    parent.spawn((
-                        ImageNode::new(asset_server.load("sprites/ball_blue_large.png"))
-                            .with_mode(NodeImageMode::Auto),
-                        Node {
-                            width: Val::Px(64.0),
-                            height: Val::Px(64.0),
-                            margin: UiRect {
-                                left: Val::Px(8.0),
-                                right: Val::Px(8.0),
-                                top: Val::Px(8.0),
-                                bottom: Val::Px(8.0),
-                            },
-                            ..Default::default()
-                        },
-                    ));
-                    //  Text
-                    parent.spawn((
-                        Text::new("Bevy Ball Game"),
-                        TextFont {
-                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                            font_size: 64.0,
-                            ..Default::default()
-                        },
-                        TextColor(Color::WHITE),
-                        TextLayout::new_with_justify(JustifyText::Center),
-                    ));
-                    // Image
-                    parent.spawn((
-                        ImageNode::new(asset_server.load("sprites/ball_red_large.png"))
-                            .with_mode(NodeImageMode::Auto),
-                        Node {
-                            width: Val::Px(64.0),
-                            height: Val::Px(64.0),
-                            margin: UiRect {
-                                left: Val::Px(8.0),
-                                right: Val::Px(8.0),
-                                top: Val::Px(8.0),
-                                bottom: Val::Px(8.0),
-                            },
-                            ..Default::default()
-                        },
-                    ));
-                });
+            parent.spawn(title_style()).with_children(|parent| {
+                // Image
+                parent.spawn((
+                    ImageNode::new(asset_server.load("sprites/ball_blue_large.png"))
+                        .with_mode(NodeImageMode::Auto),
+                    image_style(),
+                ));
+                //  Text
+                parent.spawn((
+                    Text::new("Bevy Ball Game"),
+                    text_font_64(&asset_server),
+                    TextColor(Color::WHITE),
+                    TextLayout::new_with_justify(JustifyText::Center),
+                ));
+                // Image
+                parent.spawn((
+                    ImageNode::new(asset_server.load("sprites/ball_red_large.png"))
+                        .with_mode(NodeImageMode::Auto),
+                    image_style(),
+                ));
+            });
             // ======= PLAY BUTTON =======
             parent
                 .spawn((
                     Button,
-                    Node {
-                        align_items: AlignItems::Center,
-                        justify_content: JustifyContent::Center,
-                        width: Val::Px(200.0),
-                        height: Val::Px(80.0),
-                        ..Default::default()
-                    },
+                    button_style(),
                     BackgroundColor(NORMAL_BUTTON_COLOR),
                     PlayButton {},
                 ))
                 .with_children(|parent| {
                     parent.spawn((
                         Text::new("Play"),
-                        TextFont {
-                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                            font_size: 32.0,
-                            ..Default::default()
-                        },
+                        text_font_32(&asset_server),
                         TextColor(Color::WHITE),
                         TextLayout::new_with_justify(JustifyText::Center),
                     ));
@@ -114,24 +59,14 @@ pub fn build_main_menu(commands: &mut Commands, asset_server: &Res<AssetServer>)
             parent
                 .spawn((
                     Button,
-                    Node {
-                        align_items: AlignItems::Center,
-                        justify_content: JustifyContent::Center,
-                        width: Val::Px(200.0),
-                        height: Val::Px(80.0),
-                        ..Default::default()
-                    },
+                    button_style(),
                     BackgroundColor(NORMAL_BUTTON_COLOR),
                     QuitButton {},
                 ))
                 .with_children(|parent| {
                     parent.spawn((
                         Text::new("Quit"),
-                        TextFont {
-                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                            font_size: 32.0,
-                            ..Default::default()
-                        },
+                        text_font_32(&asset_server),
                         TextColor(Color::WHITE),
                         TextLayout::new_with_justify(JustifyText::Center),
                     ));
