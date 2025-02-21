593b4fc5f07c6d20ed0be0b66ae686b91f270adb
Erick-Erdiansyah <erdiansyah290@gmail.com>
2025-02-21 20:57:11 +0700

    add placeholder for some game menu ui

--- src/main_menu/systems/layouts.rs
+++ src/main_menu/systems/layouts.rs
@@ -6,7 +6,7 @@ use crate::main_menu::{
 };
 
 pub fn spawn_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let main_menu_entity = build_main_menu(&mut commands, &asset_server);
+    let _main_menu_entity = build_main_menu(&mut commands, &asset_server);
 }
 pub fn despawn_main_menu(mut commands: Commands, main_menu_query: Query<Entity, With<MainMenu>>) {
     if let Ok(main_menu_entity) = main_menu_query.get_single() {
